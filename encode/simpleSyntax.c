d83b9e327cdabdbb3b4c60f286b8c5444f0f25af
Pedro Cunha <pedrocunha1596@gmail.com>
2018-02-02 22:06:01 +0000

    simpleSyntax quase finalizado

--- encode/simpleSyntax.c
+++ encode/simpleSyntax.c
@@ -7,21 +7,47 @@ SimpleSyntax_t* insert_value(long value, SimpleSyntax_t* simple) {
   return simple;
 }
 
-SimpleSyntax_t* insert_string(OCTET_STRING_t value, SimpleSyntax_t* simple) {
+SimpleSyntax_t* insert_string(OCTET_STRING_t* value, SimpleSyntax_t* simple) {
   simple->present = SimpleSyntax_PR_string_value;
-  simple->choice.string_value = value;
+  simple->choice.string_value = *value;
   return simple;
 }
 
-SimpleSyntax_t* insert_oid(OBJECT_IDENTIFIER_t value, SimpleSyntax_t* simple) {
+SimpleSyntax_t* insert_oid(OBJECT_IDENTIFIER_t* value, SimpleSyntax_t* simple) {
   simple->present = SimpleSyntax_PR_objectID_value;
-  simple->choice.objectID_value  = value;
+  simple->choice.objectID_value  = *value;
   return simple;
 }
 
-SimpleSyntax_t* create_simple(int flag, char* string) {
-  long value;
+OBJECT_IDENTIFIER_t* encode_OID(OBJECT_IDENTIFIER_t *oid, const char *oid_text) {
+  oid = calloc(1, sizeof(OBJECT_IDENTIFIER_t));
+  asn_oid_arc_t fixed_arcs[10];
+  asn_oid_arc_t *arcs = fixed_arcs;
+  size_t arc_slots = sizeof(fixed_arcs)/sizeof(fixed_arcs[0]);
+  ssize_t count;
+  count = OBJECT_IDENTIFIER_get_arcs(oid, arcs, arc_slots);
+  int error;
+  if(count > arc_slots) {
+    arc_slots = count;
+   	arcs = malloc(sizeof(asn_oid_arc_t) * arc_slots);
+   	if(!arcs) {
+      printf("%s\n", "Erro");
+    }
+   	count = OBJECT_IDENTIFIER_get_arcs(oid, arcs, arc_slots);
+ 		assert(count == arc_slots);
+  }
+  error = OBJECT_IDENTIFIER_set_arcs(oid,arcs,count);
+  if (error < 0) {
+    printf("%s\n", "Erro na criação do OBJECT_IDENTIFIER_t necessário para codificar OID");
+  }
+  return oid;
+}
+
+SimpleSyntax_t* create_simple(int flag, const char* string) {
   SimpleSyntax_t* simple;
+  long value;
+  OCTET_STRING_t *str;
+  OBJECT_IDENTIFIER_t *oid;
   simple = calloc(1, sizeof(SimpleSyntax_t));
   switch(flag) {
    case 1:
@@ -29,10 +55,13 @@ SimpleSyntax_t* create_simple(int flag, char* string) {
       simple = insert_value(value, simple);
       break;
    case 2:
-      //caso para a string, formato OCTET_STRING_t
+      str = calloc(1, sizeof(OCTET_STRING_t));
+      int oc = OCTET_STRING_fromBuf(str,string,-1);
+      simple = insert_string(str, simple);
       break;
    case 3:
-      //caso
+      oid = encode_OID(oid, string);
+      simple = insert_oid(oid, simple);
       break;
     }
     return simple;
